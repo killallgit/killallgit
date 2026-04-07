8e4cdaf3132910ceb8d331b655b7ba1373bdcd27
archae0pteryx <github@pocketcereal.com>
2026-04-07 15:11:40 -0700

    [SaveSystem]: Implement JsonFileSaveProvider with file I/O

--- Source/KillAllGit/SaveSystem/JsonFileSaveProvider.cpp
+++ Source/KillAllGit/SaveSystem/JsonFileSaveProvider.cpp
@@ -1,4 +1,11 @@
 #include "JsonFileSaveProvider.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
+#include "Dom/JsonObject.h"
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
+#include "Serialization/JsonWriter.h"
+#include "HAL/PlatformFileManager.h"
 
 void UJsonFileSaveProvider::SetSaveDirectory(const FString& InDirectory)
 {
@@ -7,25 +14,87 @@ void UJsonFileSaveProvider::SetSaveDirectory(const FString& InDirectory)
 
 FString UJsonFileSaveProvider::GetSaveFilePath() const
 {
-	return FString();
+	const FString Dir = SaveDirectory.IsEmpty()
+		? FPaths::ProjectSavedDir() / TEXT("GameData")
+		: SaveDirectory;
+	return Dir / TEXT("save.json");
 }
 
 bool UJsonFileSaveProvider::HasSaveData() const
 {
-	return false;
+	return FPaths::FileExists(GetSaveFilePath());
 }
 
 FSaveDataPayload UJsonFileSaveProvider::LoadSaveData() const
 {
-	return FSaveDataPayload();
+	FSaveDataPayload Payload;
+
+	FString JsonString;
+	if (!FFileHelper::LoadFileToString(JsonString, *GetSaveFilePath()))
+	{
+		return Payload;
+	}
+
+	TSharedPtr<FJsonObject> JsonObject;
+	const TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(JsonString);
+	if (!FJsonSerializer::Deserialize(Reader, JsonObject) || !JsonObject.IsValid())
+	{
+		return Payload;
+	}
+
+	Payload.Variant = GameVariantUtils::StringToVariant(
+		JsonObject->GetStringField(TEXT("variant"))
+	);
+
+	const TSharedPtr<FJsonObject>* GitHubDataObject;
+	if (JsonObject->TryGetObjectField(TEXT("githubData"), GitHubDataObject))
+	{
+		FString GitHubDataString;
+		const TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&GitHubDataString);
+		FJsonSerializer::Serialize(GitHubDataObject->ToSharedRef(), Writer);
+		Payload.GitHubDataJson = GitHubDataString;
+	}
+
+	FString CreatedAtString;
+	if (JsonObject->TryGetStringField(TEXT("createdAt"), CreatedAtString))
+	{
+		FDateTime::ParseIso8601(*CreatedAtString, Payload.CreatedAt);
+	}
+
+	return Payload;
 }
 
 bool UJsonFileSaveProvider::CreateSaveData(EGameVariant Variant, const FString& GitHubDataJson)
 {
-	return false;
+	const TSharedRef<FJsonObject> RootObject = MakeShared<FJsonObject>();
+	RootObject->SetStringField(TEXT("variant"), GameVariantUtils::VariantToString(Variant));
+	RootObject->SetStringField(TEXT("createdAt"), FDateTime::UtcNow().ToIso8601());
+
+	TSharedPtr<FJsonObject> GitHubDataObject;
+	const TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(GitHubDataJson);
+	if (FJsonSerializer::Deserialize(Reader, GitHubDataObject) && GitHubDataObject.IsValid())
+	{
+		RootObject->SetObjectField(TEXT("githubData"), GitHubDataObject);
+	}
+
+	FString OutputString;
+	const TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&OutputString);
+	FJsonSerializer::Serialize(RootObject, Writer);
+
+	const FString FilePath = GetSaveFilePath();
+	const FString Directory = FPaths::GetPath(FilePath);
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	PlatformFile.CreateDirectoryTree(*Directory);
+
+	return FFileHelper::SaveStringToFile(OutputString, *FilePath);
 }
 
 bool UJsonFileSaveProvider::DeleteSaveData()
 {
-	return false;
+	const FString FilePath = GetSaveFilePath();
+	if (!FPaths::FileExists(FilePath))
+	{
+		return true;
+	}
+	return IFileManager::Get().Delete(*FilePath);
 }
